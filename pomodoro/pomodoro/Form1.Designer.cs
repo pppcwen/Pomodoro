b29db6d9f6580ed1a775102882d40832d14852ec
LAPTOP-TOMQSL39\barry <barry360125738@gmail.com>
2023-08-16 19:33:15 +0800

    完成

--- pomodoro/pomodoro/Form1.Designer.cs
+++ pomodoro/pomodoro/Form1.Designer.cs
@@ -37,6 +37,8 @@
             sec_textbar = new TextBox();
             timer1 = new System.Windows.Forms.Timer(components);
             label1 = new Label();
+            label2 = new Label();
+            label4 = new Label();
             SuspendLayout();
             // 
             // start_stop_button
@@ -47,9 +49,10 @@
             start_stop_button.FlatAppearance.BorderSize = 0;
             start_stop_button.FlatStyle = FlatStyle.Flat;
             start_stop_button.Font = new Font("MV Boli", 13F, FontStyle.Bold, GraphicsUnit.Point);
-            start_stop_button.Location = new Point(210, 731);
+            start_stop_button.Location = new Point(135, 462);
+            start_stop_button.Margin = new Padding(2);
             start_stop_button.Name = "start_stop_button";
-            start_stop_button.Size = new Size(162, 92);
+            start_stop_button.Size = new Size(125, 64);
             start_stop_button.TabIndex = 0;
             start_stop_button.Text = "Start";
             start_stop_button.UseVisualStyleBackColor = false;
@@ -63,9 +66,10 @@
             reset_button.FlatAppearance.BorderSize = 0;
             reset_button.FlatStyle = FlatStyle.Flat;
             reset_button.Font = new Font("MV Boli", 13F, FontStyle.Bold, GraphicsUnit.Point);
-            reset_button.Location = new Point(434, 726);
+            reset_button.Location = new Point(288, 462);
+            reset_button.Margin = new Padding(2);
             reset_button.Name = "reset_button";
-            reset_button.Size = new Size(146, 101);
+            reset_button.Size = new Size(119, 67);
             reset_button.TabIndex = 3;
             reset_button.Text = "Reset";
             reset_button.UseVisualStyleBackColor = false;
@@ -79,9 +83,10 @@
             SetTime_button.FlatAppearance.BorderSize = 0;
             SetTime_button.FlatStyle = FlatStyle.Flat;
             SetTime_button.Font = new Font("MV Boli", 13F, FontStyle.Bold, GraphicsUnit.Point);
-            SetTime_button.Location = new Point(636, 722);
+            SetTime_button.Location = new Point(411, 457);
+            SetTime_button.Margin = new Padding(2);
             SetTime_button.Name = "SetTime_button";
-            SetTime_button.Size = new Size(182, 101);
+            SetTime_button.Size = new Size(170, 71);
             SetTime_button.TabIndex = 4;
             SetTime_button.Text = "Set Time";
             SetTime_button.UseVisualStyleBackColor = false;
@@ -89,47 +94,56 @@
             // 
             // hr_textbar
             // 
+            hr_textbar.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             hr_textbar.BackColor = Color.FromArgb(240, 64, 32);
-            hr_textbar.BorderStyle = BorderStyle.None;
             hr_textbar.Font = new Font("MV Boli", 32F, FontStyle.Bold, GraphicsUnit.Point);
-            hr_textbar.Location = new Point(258, 392);
+            hr_textbar.Location = new Point(126, 267);
+            hr_textbar.Margin = new Padding(2);
+            hr_textbar.MaxLength = 2;
             hr_textbar.Multiline = true;
             hr_textbar.Name = "hr_textbar";
-            hr_textbar.Size = new Size(170, 112);
+            hr_textbar.Size = new Size(115, 81);
             hr_textbar.TabIndex = 7;
-            hr_textbar.Text = "52";
+            hr_textbar.Text = "0";
             hr_textbar.TextAlign = HorizontalAlignment.Center;
             hr_textbar.TextChanged += hr_textbar_TextChanged;
+            hr_textbar.KeyPress += hr_textbar_KeyPress;
             // 
             // min_textbar
             // 
             min_textbar.BackColor = Color.FromArgb(240, 64, 32);
-            min_textbar.BorderStyle = BorderStyle.None;
             min_textbar.Font = new Font("MV Boli", 32F, FontStyle.Bold, GraphicsUnit.Point);
-            min_textbar.Location = new Point(434, 392);
+            min_textbar.Location = new Point(275, 267);
+            min_textbar.Margin = new Padding(2);
+            min_textbar.MaxLength = 2;
             min_textbar.Multiline = true;
             min_textbar.Name = "min_textbar";
-            min_textbar.Size = new Size(174, 96);
+            min_textbar.Size = new Size(119, 81);
             min_textbar.TabIndex = 8;
-            min_textbar.Text = "52";
+            min_textbar.Text = "0";
             min_textbar.TextAlign = HorizontalAlignment.Center;
+            min_textbar.TextChanged += min_textbar_TextChanged;
+            min_textbar.KeyPress += min_textbar_KeyPress;
             // 
             // sec_textbar
             // 
             sec_textbar.BackColor = Color.FromArgb(240, 64, 32);
-            sec_textbar.BorderStyle = BorderStyle.None;
             sec_textbar.Font = new Font("MV Boli", 32F, FontStyle.Bold, GraphicsUnit.Point);
-            sec_textbar.Location = new Point(614, 392);
+            sec_textbar.Location = new Point(431, 267);
+            sec_textbar.Margin = new Padding(2);
+            sec_textbar.MaxLength = 2;
             sec_textbar.Multiline = true;
             sec_textbar.Name = "sec_textbar";
-            sec_textbar.Size = new Size(182, 96);
+            sec_textbar.Size = new Size(111, 81);
             sec_textbar.TabIndex = 9;
-            sec_textbar.Text = "52";
+            sec_textbar.Text = "0";
             sec_textbar.TextAlign = HorizontalAlignment.Center;
             sec_textbar.TextChanged += sec_textbar_TextChanged;
+            sec_textbar.KeyPress += sec_textbar_KeyPress;
             // 
             // timer1
             // 
+            timer1.Interval = 1000;
             timer1.Tick += timer1_Tick;
             // 
             // label1
@@ -138,20 +152,49 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("MV Boli", 32F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Location = new Point(346, 288);
+            label1.Location = new Point(146, 185);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(387, 85);
+            label1.Size = new Size(324, 70);
             label1.TabIndex = 10;
             label1.Text = "Keep going!";
             // 
+            // label2
+            // 
+            label2.AutoEllipsis = true;
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft JhengHei UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(246, 278);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 43);
+            label2.TabIndex = 11;
+            label2.Text = ":";
+            label2.UseMnemonic = false;
+            // 
+            // label4
+            // 
+            label4.AutoEllipsis = true;
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Microsoft JhengHei UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(399, 278);
+            label4.Name = "label4";
+            label4.Size = new Size(27, 43);
+            label4.TabIndex = 13;
+            label4.Text = ":";
+            label4.UseMnemonic = false;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(14F, 30F);
+            AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             BackgroundImage = Properties.Resources.pomodoro_photo6;
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(1044, 851);
+            ClientSize = new Size(671, 539);
+            Controls.Add(label4);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(sec_textbar);
             Controls.Add(min_textbar);
@@ -162,6 +205,7 @@
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Location = new Point(1200, 440);
+            Margin = new Padding(2);
             Name = "Form1";
             StartPosition = FormStartPosition.Manual;
             Text = "Form1";
@@ -180,5 +224,7 @@
         private TextBox sec_textbar;
         private System.Windows.Forms.Timer timer1;
         private Label label1;
+        private Label label2;
+        private Label label4;
     }
 }
--- pomodoro/pomodoro/Form1.cs
+++ pomodoro/pomodoro/Form1.cs
@@ -15,7 +15,8 @@ namespace pomodoro
 
         }
 
-        int hr, min, sec;
+        //定義時間
+        int hr, min, sec,ms;
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -32,12 +33,17 @@ namespace pomodoro
         }
 
 
-
-
-
         private void sec_textbar_TextChanged(object sender, EventArgs e)
         {
-
+            //讀取輸入字串並轉為數字
+            int.TryParse(sec_textbar.Text, out sec);
+            //防止數字超出上限
+            if (sec > 60)
+            {
+                sec = 60;
+                string secstr = sec.ToString();
+                sec_textbar.Text = secstr;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -47,11 +53,17 @@ namespace pomodoro
 
         private void SetTime_button_Click(object sender, EventArgs e)
         {
+            //設置時間
             if (SetTime_button.Text == "Set Time")
             {
                 hr_textbar.Enabled = true;
                 min_textbar.Enabled = true;
                 sec_textbar.Enabled = true;
+                hr_textbar.Text = "";
+                min_textbar.Text = "";
+                sec_textbar.Text = "";
+                start_stop_button.Enabled = false;
+                reset_button.Enabled = false;
                 SetTime_button.Text = "Ok";
             }
             else if (SetTime_button.Text == "Ok")
@@ -60,12 +72,15 @@ namespace pomodoro
                 hr_textbar.Enabled = false;
                 min_textbar.Enabled = false;
                 sec_textbar.Enabled = false;
+                start_stop_button.Enabled = true;
+                reset_button.Enabled = true;
             }
 
         }
 
         private void reset_button_Click(object sender, EventArgs e)
         {
+            //將所有數字重置
             timer1.Stop();
             hr = 0;
             min = 0;
@@ -76,40 +91,54 @@ namespace pomodoro
             hr_textbar.Text = hrstr;
             min_textbar.Text = minstr;
             sec_textbar.Text = secstr;
+            label1.Text = "";
         }
 
         private void hr_textbar_TextChanged(object sender, EventArgs e)
         {
-
+            //讀取輸入字串並轉為數字
+            int.TryParse(hr_textbar.Text, out hr);
+            //防止數字超出上限
+            if (hr > 24)
+            {
+                hr = 24;
+                string hrstr = hr.ToString();
+                hr_textbar.Text = hrstr;
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (sec == 0)
-            {
-                if (min == 0)
+            //顯示時間運作
+                if (sec == 0)
                 {
-                    if (hr == 0)
+                    if (min == 0)
                     {
-                        timer1.Stop();
+                        if (hr == 0)
+                        {
+                            timer1.Stop();
+                        }
+                        else
+                        {
+                            hr -= 1;
+                            min = 59;
+                            sec = 59;
+                        
+                        }
                     }
                     else
                     {
-                        hr -= 1;
-                        min = 59;
+                        min -= 1;
                         sec = 59;
+                    
                     }
                 }
                 else
                 {
-                    min -= 1;
-                    sec = 59;
+                    sec -= 1;
+                
                 }
-            }
-            else
-            {
-                sec -= 1;
-            }
+        
             string hrstr = hr.ToString();
             string minstr = min.ToString();
             string secstr = sec.ToString();
@@ -118,7 +147,7 @@ namespace pomodoro
             sec_textbar.Text = secstr;
 
             //判斷顯示甚麼字
-            if (sec == 0 || min == 0 || hr == 0)
+            if (sec == 0 && min == 0 && hr == 0)
             {
                 label1.Text = "Congratulations!";
             }
@@ -128,6 +157,49 @@ namespace pomodoro
             }
         }
 
+        private void label3_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void sec_textbar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //只允許在秒數格輸入數字
+            if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void hr_textbar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //只允許在分鐘格輸入數字
+            if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void min_textbar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //只允許在小時格輸入數字
+            if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void min_textbar_TextChanged(object sender, EventArgs e)
+        {
+            //讀取輸入字串並轉為數字
+            int.TryParse(min_textbar.Text, out min);
+            //防止數字超出上限
+            if (min > 60)
+            {
+                min = 60;
+                string minstr = min.ToString();
+                min_textbar.Text = minstr;
+            }
+        }
     }
 }
